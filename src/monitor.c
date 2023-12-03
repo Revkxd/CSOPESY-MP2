5c4883f1c132a54b56f18cff8fd4f782717578d0
zen-young <renzcruz50@gmail.com>
2023-12-03 14:58:23 +0800

    fixed bug of not incrementing tail

--- src/monitor.c
+++ src/monitor.c
@@ -5,7 +5,7 @@
 void fifo_monitor_lock(fifo_monitor_t *monitor)
 {
     pthread_mutex_lock(&monitor->lock);
-    size_t thread_idx = monitor->tail;
+    size_t thread_idx = monitor->tail++;
     while (thread_idx != monitor->head)
         pthread_cond_wait(&monitor->cond, &monitor->lock);
     pthread_mutex_unlock(&monitor->lock);
